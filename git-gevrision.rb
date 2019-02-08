858a93d8d81488e50d57ce9f913e1b05642aac80
kyb <i.kyb@ya.ru>
2019-02-09 01:00:48 +0200

    + desc, sha256

--- git-gevrision.rb
+++ git-gevrision.rb
@@ -1,15 +1,18 @@
 require 'formula'
 
 class GitGevrision < Formula
-
-    homepage 'https://gitlab.com/kyb/git-gevrision'
+	desc 'Gives information about Git repository revision in format like \'master-c73-gbbb6bec\'. Can fill template string or file. Useful to provide information about version of the program: branch, tag, commit hash, commits count, dirty status.'
+	homepage 'https://gitlab.com/kyb/git-gevrision'
 	url 'https://gitlab.com/kyb/git-gevrision/raw/master/git-gevrision.sh?inline=false'
-    #sha256 '0b528ed24db9f0fa39e5efdae9bcfc56bf9e07555cb267c70ff3fee84ec98460'
+	sha256 'b9564e4c3aa8079d1749c4cffcb58130fe5c92dc1fbf76e9c4dbd2b538420d79'
 	version 'latest'
-    #version 'master-c91-gd587f03'
-    
-    def install
-        system 'chmod','+x','git-gevrision.sh' 
-        bin.install 'git-gevrision.sh'
-    end
+	#version 'master-c91-gd587f03'
+	
+	#depends_on "git" => :recommended  ## Usually installed with developer tools.
+
+	def install
+		system 'chmod','+x','git-gevrision.sh' 
+		system 'mv','git-gevrision.sh','git-gevrision' 
+		bin.install 'git-gevrision'
+	end
 end
