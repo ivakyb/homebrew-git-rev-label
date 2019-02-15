e5c7e794d9dae5e5c967021083264b2afccb451e
kyb <i.kyb@ya.ru>
2019-02-15 22:32:21 +0200

    Update git-rev-label.rb

--- git-rev-label.rb
+++ git-rev-label.rb
@@ -1,6 +1,6 @@
 require 'formula'
 
-class GitGevrision < Formula
+class GitRevLabel < Formula
 	desc 'Gives information about Git repository revision in format like \'master-c73-gbbb6bec\'. Can fill template string or file with env vars and information from Git. Useful to provide information about version of the program: branch, tag, commit hash, commits count, dirty status.'
 	homepage 'https://gitlab.com/kyb/git-rev-label'
 	url 'https://gitlab.com/kyb/git-rev-label/raw/master/git-rev-label.sh?inline=false'
