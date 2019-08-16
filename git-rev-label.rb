a212f99dc1f0c4f724d01eac9d77d7e70e7e2216
git-rev-label.ci <git-rev-label.ci@gitlab.com>
2019-08-16 20:37:26 +0000

    homebrew-c118-g18cd136-b102

--- git-rev-label.rb
+++ git-rev-label.rb
@@ -4,8 +4,8 @@ class GitRevLabel < Formula
 	desc 'Gives information about Git repository revision in format like \'master-c73-gbbb6bec\'. Can fill template string or file with env vars and information from Git. Useful to provide information about version of the program: branch, tag, commit hash, commits count, dirty status.'
 	homepage 'https://gitlab.com/kyb/git-rev-label'
 	url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/homebrew/git-rev-label'
-	sha256 '0308f2ca9d819c1ed17557d5c3a2aa178bef823c441cf03f959a94b5d1a0f585'
-	version 'homebrew-c118-gc01f9c6-b101'
+	sha256 '8f801edfda976460bd3b25b31b53bf38227176765ec1680657bcf9195b5e44b4'
+	version '1.118.102'
 	
 	#depends_on "git" => :recommended  ## Usually installed with developer tools.
 
