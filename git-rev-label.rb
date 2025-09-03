fe9461a24691397f8fb203a921e9e5f4bb931e70
git-rev-label.ci <git-rev-label.ci@gitlab.com>
2025-09-03 10:42:19 +0000

    master-c26-g033348f-b24

--- git-rev-label.rb
+++ git-rev-label.rb
@@ -1,16 +1,21 @@
 require 'formula'
 
+## The GitRevLabel formula provides Git repository revision information
+## in a formatted string format, useful for version tracking
 class GitRevLabel < Formula
-	desc 'Gives information about Git repository revision in format like \'master-c73-gbbb6bec\'. Can fill template string or file with env vars and information from Git. Useful to provide information about version of the program: branch, tag, commit hash, commits count, dirty status.'
-	homepage 'https://gitlab.com/kyb/git-rev-label'
-	url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/master/git-rev-label'
-	sha256 '9ea445973b6252ef827ba10dc81167f0d45892426b687ee9801834201b93ac08'
-	version '2.16.14'
-	
-	#depends_on "git" => :recommended  ## Usually installed with developer tools.
+  desc 'Gives information about Git repository revision in format like ' \
+       '\'master-c73-gbbb6bec\'. Can fill template string or file with env vars ' \
+       'and information from Git. Useful to provide information about version of the ' \
+       'program: branch, tag, commit hash, commits count, dirty status.'
+  homepage 'https://gitlab.com/kyb/git-rev-label'
+  url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/master/git-rev-label'
+  sha256 '3a48b252ece117e24932769069e6840799361cedebaa92a40706dcddcb7fb514'
+  version '2.26.24'
 
-	def install
-		system 'chmod','+x','git-rev-label' 
-		bin.install 'git-rev-label'
-	end
+  # depends_on "git" => :recommended  ## Usually installed with developer tools.
+
+  def install
+    system 'chmod', '+x', 'git-rev-label'
+    bin.install 'git-rev-label'
+  end
 end
