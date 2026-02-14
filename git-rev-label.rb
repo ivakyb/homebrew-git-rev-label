bdb2e8e3d8c5fc49219e9bce92da8553ccd826ef
git-rev-label.ci <git-rev-label.ci@gitlab.com>
2026-02-14 23:06:19 +0000

    master-c27-g1eb4c9d-b49

--- git-rev-label.rb
+++ git-rev-label.rb
@@ -9,8 +9,8 @@ class GitRevLabel < Formula
        'program: branch, tag, commit hash, commits count, dirty status.'
   homepage 'https://gitlab.com/kyb/git-rev-label'
   url 'https://gitlab.com/kyb/git-rev-label/raw/artifacts/master/git-rev-label'
-  sha256 '9164d5da25a2d6cfa514f7a00f8b72cb06ede327c8c59d29919d626d31fd3850'
-  version '2.27.48'
+  sha256 '8003b349d54affdc3745f38de2ea35406fe254656e844ecb4fd3811eef87e5bf'
+  version '2.27.49'
 
   # depends_on "git" => :recommended  ## Usually installed with developer tools.
 
